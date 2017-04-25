1eb98ed69568d2a4bc1d15667f7cb4e9910060cb
Kevin Leung <kevinresol@gmail.com>
2017-04-25 17:36:23 +0800

    toString() is unncessary

--- tests/SourceTest.hx
+++ tests/SourceTest.hx
@@ -14,10 +14,10 @@ class SourceTest {
 		var s1:IdealSource = '01234';
 		var s2:IdealSource = '56789';
 		
-		s1.all().handle(function(c) asserts.assert(c.toString() == '01234'));
-		s2.all().handle(function(c) asserts.assert(c.toString() == '56789'));
-		s1.append(s2).all().handle(function(c) asserts.assert(c.toString() == '0123456789'));
-		s2.append(s1).all().handle(function(c) asserts.assert(c.toString() == '5678901234'));
+		s1.all().handle(function(chunk) asserts.assert(chunk == '01234'));
+		s2.all().handle(function(chunk) asserts.assert(chunk == '56789'));
+		s1.append(s2).all().handle(function(chunk) asserts.assert(chunk == '0123456789'));
+		s2.append(s1).all().handle(function(chunk) asserts.assert(chunk == '5678901234'));
 		
 		return asserts.done();
 	}
@@ -26,10 +26,10 @@ class SourceTest {
 		var s1:IdealSource = '01234';
 		var s2:IdealSource = '56789';
 		
-		s1.all().handle(function(c) asserts.assert(c.toString() == '01234'));
-		s2.all().handle(function(c) asserts.assert(c.toString() == '56789'));
-		s1.prepend(s2).all().handle(function(c) asserts.assert(c.toString() == '5678901234'));
-		s2.prepend(s1).all().handle(function(c) asserts.assert(c.toString() == '0123456789'));
+		s1.all().handle(function(chunk) asserts.assert(chunk == '01234'));
+		s2.all().handle(function(chunk) asserts.assert(chunk == '56789'));
+		s1.prepend(s2).all().handle(function(chunk) asserts.assert(chunk == '5678901234'));
+		s2.prepend(s1).all().handle(function(chunk) asserts.assert(chunk == '0123456789'));
 		
 		return asserts.done();
 	}
@@ -38,9 +38,9 @@ class SourceTest {
 		var s1:IdealSource = '01234';
 		var s2:IdealSource = '56789';
 		
-		s1.append(s2).skip(3).all().handle(function(o) asserts.assert(o.toString() == '3456789'));
-		s1.append(s2).skip(5).all().handle(function(o) asserts.assert(o.toString() == '56789'));
-		s1.append(s2).skip(7).all().handle(function(o) asserts.assert(o.toString() == '789'));
+		s1.append(s2).skip(3).all().handle(function(chunk) asserts.assert(chunk == '3456789'));
+		s1.append(s2).skip(5).all().handle(function(chunk) asserts.assert(chunk == '56789'));
+		s1.append(s2).skip(7).all().handle(function(chunk) asserts.assert(chunk == '789'));
 		
 		return asserts.done();
 	}
@@ -49,9 +49,9 @@ class SourceTest {
 		var s1:IdealSource = '01234';
 		var s2:IdealSource = '56789';
 		
-		s1.append(s2).limit(3).all().handle(function(o) asserts.assert(o.toString() == '012'));
-		s1.append(s2).limit(5).all().handle(function(o) asserts.assert(o.toString() == '01234'));
-		s1.append(s2).limit(7).all().handle(function(o) asserts.assert(o.toString() == '0123456'));
+		s1.append(s2).limit(3).all().handle(function(chunk) asserts.assert(chunk == '012'));
+		s1.append(s2).limit(5).all().handle(function(chunk) asserts.assert(chunk == '01234'));
+		s1.append(s2).limit(7).all().handle(function(chunk) asserts.assert(chunk == '0123456'));
 		
 		return asserts.done();
 	}
@@ -60,13 +60,13 @@ class SourceTest {
 		var s1:IdealSource = '01234';
 		var s2:IdealSource = '56789';
 		
-		s1.append(s2).skip(1).limit(3).all().handle(function(o) asserts.assert(o.toString() == '123'));
-		s1.append(s2).skip(6).limit(3).all().handle(function(o) asserts.assert(o.toString() == '678'));
-		s1.append(s2).skip(2).limit(6).all().handle(function(o) asserts.assert(o.toString() == '234567'));
+		s1.append(s2).skip(1).limit(3).all().handle(function(chunk) asserts.assert(chunk == '123'));
+		s1.append(s2).skip(6).limit(3).all().handle(function(chunk) asserts.assert(chunk == '678'));
+		s1.append(s2).skip(2).limit(6).all().handle(function(chunk) asserts.assert(chunk == '234567'));
 		
-		s1.append(s2).limit(4).skip(2).all().handle(function(o) asserts.assert(o.toString() == '23'));
-		s1.append(s2).limit(8).skip(3).all().handle(function(o) asserts.assert(o.toString() == '34567'));
-		s1.append(s2).limit(8).skip(6).all().handle(function(o) asserts.assert(o.toString() == '67'));
+		s1.append(s2).limit(4).skip(2).all().handle(function(chunk) asserts.assert(chunk == '23'));
+		s1.append(s2).limit(8).skip(3).all().handle(function(chunk) asserts.assert(chunk == '34567'));
+		s1.append(s2).limit(8).skip(6).all().handle(function(chunk) asserts.assert(chunk == '67'));
 		
 		return asserts.done();
 	}
@@ -76,39 +76,39 @@ class SourceTest {
 		var s2:IdealSource = '56789';
 		
 		var split = s1.append(s2).split('45');
-		split.before.all().handle(function(chunk) asserts.assert(chunk.toString() == '0123'));
-		split.after.all().handle(function(chunk) asserts.assert(chunk.toString() == '6789'));
+		split.before.all().handle(function(chunk) asserts.assert(chunk == '0123'));
+		split.after.all().handle(function(chunk) asserts.assert(chunk == '6789'));
 		split.delimiter.handle(function(o) asserts.assert(o.orUse(Chunk.EMPTY) == '45'));
 		
 		var s1:IdealSource = '12';
 		var split = s1.split('2');
-		split.before.all().handle(function(chunk) asserts.assert(chunk.toString() == '1'));
-		split.after.all().handle(function(chunk) asserts.assert(chunk.toString() == ''));
+		split.before.all().handle(function(chunk) asserts.assert(chunk == '1'));
+		split.after.all().handle(function(chunk) asserts.assert(chunk == ''));
 		split.delimiter.handle(function(o) asserts.assert(o.orUse(Chunk.EMPTY) == '2'));
 		
 		var split = s1.split('1');
-		split.before.all().handle(function(chunk) asserts.assert(chunk.toString() == ''));
-		split.after.all().handle(function(chunk) asserts.assert(chunk.toString() == '2'));
+		split.before.all().handle(function(chunk) asserts.assert(chunk == ''));
+		split.after.all().handle(function(chunk) asserts.assert(chunk == '2'));
 		split.delimiter.handle(function(o) asserts.assert(o.orUse(Chunk.EMPTY) == '1'));
 		
 		var split = s1.split('3');
-		split.before.all().handle(function(chunk) asserts.assert(chunk.toString() == '12'));
-		split.after.all().handle(function(chunk) asserts.assert(chunk.toString() == ''));
+		split.before.all().handle(function(chunk) asserts.assert(chunk == '12'));
+		split.after.all().handle(function(chunk) asserts.assert(chunk == ''));
 		split.delimiter.handle(function(o) asserts.assert(o.orUse(Chunk.EMPTY) == ''));
 		
 		var s1:IdealSource = '12131415';
 		var split = s1.split('1');
-		split.before.all().handle(function(chunk) asserts.assert(chunk.toString() == ''));
-		split.after.all().handle(function(chunk) asserts.assert(chunk.toString() == '2131415'));
+		split.before.all().handle(function(chunk) asserts.assert(chunk == ''));
+		split.after.all().handle(function(chunk) asserts.assert(chunk == '2131415'));
 		var split = split.after.split('1');
-		split.before.all().handle(function(chunk) asserts.assert(chunk.toString() == '2'));
-		split.after.all().handle(function(chunk) asserts.assert(chunk.toString() == '31415'));
+		split.before.all().handle(function(chunk) asserts.assert(chunk == '2'));
+		split.after.all().handle(function(chunk) asserts.assert(chunk == '31415'));
 		var split = split.after.split('1');
-		split.before.all().handle(function(chunk) asserts.assert(chunk.toString() == '3'));
-		split.after.all().handle(function(chunk) asserts.assert(chunk.toString() == '415'));
+		split.before.all().handle(function(chunk) asserts.assert(chunk == '3'));
+		split.after.all().handle(function(chunk) asserts.assert(chunk == '415'));
 		var split = split.after.split('1');
-		split.before.all().handle(function(chunk) asserts.assert(chunk.toString() == '4'));
-		split.after.all().handle(function(chunk) asserts.assert(chunk.toString() == '5'));
+		split.before.all().handle(function(chunk) asserts.assert(chunk == '4'));
+		split.after.all().handle(function(chunk) asserts.assert(chunk == '5'));
 		
 		return asserts.done();
 	}
