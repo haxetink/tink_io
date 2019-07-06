fb20a9e3582a91438329b2bf0acbf49b8a8761ff
Jason O'Neil <jason.oneil@gmail.com>
2019-07-06 20:18:13 +0800

    Fix deprecated js.Error reference

--- src/tink/io/Source.hx
+++ src/tink/io/Source.hx
@@ -7,6 +7,8 @@ import tink.streams.IdealStream;
 import tink.streams.RealStream;
 import tink.streams.Stream;
 
+import #if haxe4 js.lib.Error #else js.Error #end as JsError;
+
 using tink.io.Source;
 using tink.CoreApi;
 
@@ -43,7 +45,7 @@ abstract Source<E>(SourceObject<E>) from SourceObject<E> to SourceObject<E> to S
           source = rest;
           native.once('drain', write);
         case Failed(e):
-          native.emit('error', new js.Error(e.message));
+          native.emit('error', new JsError(e.message));
       });
     }
     
