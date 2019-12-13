af1d6453501e25dcb127f4eb3b83d4db4f11109c
Kevin Leung <kevinresol@gmail.com>
2019-12-13 10:41:48 +0800

    Run pipe test on nodejs too

--- tests/RunTests.hx
+++ tests/RunTests.hx
@@ -7,7 +7,7 @@ class RunTests {
   
   static function main() {
     Runner.run(TestBatch.make([
-      #if sys new PipeTest(),#end
+      #if (sys || nodejs) new PipeTest(),#end
       new SourceTest(),
       new ParserTest(),
       #if (js && !nodejs) new JsTest(), #end
