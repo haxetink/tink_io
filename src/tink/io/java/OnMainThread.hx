54c49d5d3d7f55767cce88cfb73c9a67e27f0ead
Kevin Leung <kevinresol@gmail.com>
2026-07-21 21:52:52 +1000

    Move hacks to the haxe12985 lib

--- src/tink/io/java/OnMainThread.hx
+++ src/tink/io/java/OnMainThread.hx
@@ -1,17 +1,11 @@
 package tink.io.java;
 
 class OnMainThread {
-  public static function init() {
-    run(noop);
-  }
-
   public static function run(fn:Void->Void):Void {
     #if haxe5
-    sys.thread.Thread.main().events.run(fn);
+    haxe.EventLoop.main.run(fn);
     #else
     haxe.EntryPoint.runInMainThread(fn);
     #end
   }
-
-  static function noop() {}
 }
